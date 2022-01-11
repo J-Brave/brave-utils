6b9b42a36abf85c8caeaceeef2ff0a1bcc35a5d3
J-Brave <mrcaoqiang@163.com>
2022-01-11 22:42:36 +0800

    Initial commit

---
+++ src/main/java/com/brave/UtilApplication.java
@@ -0,0 +1,17 @@
+package com.brave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: J-Brave
+ * Date: 2022/1/11
+ * Time: 22:28
+ * Description:
+ */
+@SpringBootApplication
+public class UtilApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UtilApplication.class, args);
+    }
+}
